00f1a0e0b296943131338da06844bb0c7c64d9eb
Stjepan Zugcic <stzugcic@vsite.hr>
2017-08-06 13:42:55 +0200

    prvi_commit

--- CodeChef/Program.cs
+++ CodeChef/Program.cs
@@ -4,9 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CodeChef {
 	class Program {
 		static void Main(string[] args) {
+			Queue<String> ime;
+			string console_cases;
+			int cases;
+			Console.WriteLine("Upiši broj slučajeva->");
+			console_cases = Console.ReadLine();
+			cases = Int32.Parse(console_cases);
+			for (int i = 0;i < cases;++i) {
+				Console.WriteLine("Dobar dan\n");
+			}
 		}
 	}
 }
